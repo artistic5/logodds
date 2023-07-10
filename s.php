8258b4dd564147573c9f3b6c434668bef7231542
artistic5 <artisticsoftware5@gmail.com>
2023-07-10 02:36:12 +0000

    ...

---
+++ s.php
@@ -0,0 +1,53 @@
+<?php
+
+if(!isset($argv[1])) die("Race Date Not Entered!!\n");
+
+$raceDate = trim($argv[1]);
+$currentDir = __DIR__ . DIRECTORY_SEPARATOR . $raceDate;
+$outFile = $currentDir . DIRECTORY_SEPARATOR . "probas.php";
+
+$probas1 = include($currentDir . DIRECTORY_SEPARATOR . "1.php");
+$probas2 = include($currentDir . DIRECTORY_SEPARATOR . "2.php");
+$probas3 = include($currentDir . DIRECTORY_SEPARATOR . "3.php");
+
+$outtext = "<?php\n\n";
+$outtext .= "return [\n";
+
+foreach($probas1 as $raceNumber => $raceProbas1){
+    $favorite = $raceProbas1['Favorite'];
+
+    $I1 = $raceProbas1['I'];
+    $I1 = explode(", ", $I1);
+    $I2 = $probas2[$raceNumber]['I'];
+    $I2 = explode(", ", $I2);
+    $I3 = $probas3[$raceNumber]['I'];
+    $I3 = explode(", ", $I3);
+    $IIntersection = array_intersect($I1, $I2, $I3);
+    $IUnion = array_values(array_unique(array_merge($I1, $I2, $I3)));
+    sort($IIntersection);
+    sort($IUnion);
+
+    if(!empty($IIntersection)) $qplRightSide = $IIntersection;
+    else $qplRightSide = array_diff($IUnion, [$favorite]);
+
+    $qpl = "'" . $favorite . " X " . implode(", ", $qplRightSide) . "'";
+
+    $racetext = "\t'$raceNumber' => [\n";
+    $racetext .= "\t\t/**\n";
+    $racetext .= "\t\tRace $raceNumber\n";
+    $racetext .= "\t\t*/\n";
+    
+    $racetext .= "\t\t'qpl' => $qpl,\n";
+    $racetext .= "\t\t'U' => '" . implode(", ", $IUnion) . "',\n";
+    
+    $racetext .= "\t],\n";
+
+    $outtext .= $racetext;
+    
+}
+
+$outtext .= "];\n";
+
+file_put_contents($outFile, $outtext);
+
+?>
