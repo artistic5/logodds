a6910dbf4051ec229cec10b0431486a973c08e55
artistic5 <artisticsoftware5@gmail.com>
2023-08-12 05:18:30 +0000

    ...

---
+++ 0813/1.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	'2' => [
+		/**
+		Race 2
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [[14, 7, 16, 11, 4]] ,
+		'inters' =>  [[14, 7, 16, 11, 4]] ,
+		'Favorite' =>  '14',
+		'Red QPL values'      =>  '14, 7, 16',
+		'Black QPL values'    =>  '11, 4',
+		'Inter QPL' =>  '14, 7, 16, 11, 4',
+	],
+];
---
+++ 0813/2.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+	'2' => [
+		/**
+		Race 2
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [] ,
+		'inters' =>  [] ,
+		'Favorite' =>  '14',
+		'Sure Place'    =>  '',
+		'All Wins values'    =>  '',
+		'Place values'    =>  '',
+		'Red QPL values'      =>  '',
+		'Black QPL values'    =>  '',
+		'Inter Inters' =>  '',
+		'Inter QPL' =>  '',
+		'I' =>  '',
+		'Bet' =>  '',
+		'PLACE' =>  '14',
+	],
+];
