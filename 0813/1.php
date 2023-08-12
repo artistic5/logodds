31a2603d26a6ae7c3f1e081078c05e1907eaadf7
artistic5 <artisticsoftware5@gmail.com>
2023-08-12 05:59:26 +0000

    ...

--- 0813/1.php
+++ 0813/1.php
@@ -6,12 +6,12 @@ return [
 		Race 2
 		*/
 		'wins' =>  [] ,
-		'qpl/trio' =>  [[14, 7, 16, 11, 4], [14, 7, 12, 11, 6]] ,
-		'inters' =>  [[14, 7, 16, 11, 4], [14, 7, 12, 11, 6]] ,
+		'qpl/trio' =>  [[14, 7, 12, 11, 6]] ,
+		'inters' =>  [[14, 7, 12, 11, 6]] ,
 		'Favorite' =>  '14',
-		'Red QPL values'      =>  '14, 7, 12, 16',
-		'Black QPL values'    =>  '11, 6, 4',
-		'Inter QPL' =>  '14, 7, 11',
+		'Red QPL values'      =>  '14, 7, 12',
+		'Black QPL values'    =>  '11, 6',
+		'Inter QPL' =>  '14, 7, 12, 11, 6',
 	],
 	'4' => [
 		/**
@@ -19,7 +19,7 @@ return [
 		*/
 		'wins' =>  [] ,
 		'qpl/trio' =>  [[8, 6, 4, 3, 1]] ,
-		'inters' =>  [[14, 7, 16, 11, 4], [8, 6, 4, 3, 1]] ,
+		'inters' =>  [[8, 6, 4, 3, 1]] ,
 		'Favorite' =>  '8',
 		'Red QPL values'      =>  '3, 1',
 		'Black QPL values'    =>  '8, 6, 4',
