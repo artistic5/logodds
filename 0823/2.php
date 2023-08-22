3e04d51ad85b00d82d6af5ba2a4bbf17286f1df0
artistic5 <artisticsoftware5@gmail.com>
2023-08-22 06:25:17 +0000

    ...

--- 0823/2.php
+++ 0823/2.php
@@ -8,7 +8,7 @@ return [
 		'wins' =>  [] ,
 		'qpl/trio' =>  [] ,
 		'inters' =>  [] ,
-		'Favorite' =>  '17',
+		'Favorite' =>  '8',
 		'Sure Place'    =>  '',
 		'All Wins values'    =>  '',
 		'Place values'    =>  '',
