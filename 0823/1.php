1c4fc9dde04c5a2ffceb891f69a4123c98b638ba
artistic5 <artisticsoftware5@gmail.com>
2023-08-22 05:12:38 +0000

    ...

--- 0823/1.php
+++ 0823/1.php
@@ -1,4 +1,16 @@
 <?php
 
 return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'wins' =>  [[9]] ,
+		'qpl/trio' =>  [[17, 2, 8, 1, 9]] ,
+		'inters' =>  [[17, 2, 8, 1]] ,
+		'Favorite' =>  '17',
+		'Red QPL values'      =>  '1, 9',
+		'Black QPL values'    =>  '17, 2, 8',
+		'Inter QPL' =>  '17, 2, 8, 1, 9',
+	],
 ];
