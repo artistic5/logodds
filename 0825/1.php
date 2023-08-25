0448585c913724ed3351bbbc4f8500db351e8c34
artistic5 <artisticsoftware5@gmail.com>
2023-08-25 03:28:02 +0000

    ...

---
+++ 0825/1.php
@@ -0,0 +1,76 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [[11, 15, 4, 9, 1]] ,
+		'inters' =>  [[11, 15, 4, 9, 1]] ,
+		'Favorite' =>  '11',
+		'Red QPL values'      =>  '9, 1',
+		'Black QPL values'    =>  '11, 15, 4',
+		'Inter QPL' =>  '11, 15, 4, 9, 1',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [[2, 6, 8, 1, 7]] ,
+		'inters' =>  [[2, 6, 8, 1, 7]] ,
+		'Favorite' =>  '2',
+		'Red QPL values'      =>  '1, 7',
+		'Black QPL values'    =>  '2, 6, 8',
+		'Inter QPL' =>  '2, 6, 8, 1, 7',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'wins' =>  [[8]] ,
+		'qpl/trio' =>  [[10, 2, 14, 16, 8]] ,
+		'inters' =>  [[10, 2, 14, 16]] ,
+		'Favorite' =>  '8',
+		'Red QPL values'      =>  '14, 16',
+		'Black QPL values'    =>  '10, 8, 2',
+		'Inter QPL' =>  '10, 2, 14, 16, 8',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'wins' =>  [[7]] ,
+		'qpl/trio' =>  [[1, 5, 2, 11, 7]] ,
+		'inters' =>  [[1, 5, 2, 11]] ,
+		'Favorite' =>  '7',
+		'Red QPL values'      =>  '7, 1, 5',
+		'Black QPL values'    =>  '2, 11',
+		'Inter QPL' =>  '1, 5, 2, 11, 7',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [[2, 13, 15, 9, 7]] ,
+		'inters' =>  [[2, 13, 15, 9, 7]] ,
+		'Favorite' =>  '2',
+		'Red QPL values'      =>  '9, 7',
+		'Black QPL values'    =>  '2, 13, 15',
+		'Inter QPL' =>  '2, 13, 15, 9, 7',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'wins' =>  [[8, 9, 7]] ,
+		'qpl/trio' =>  [[4, 6, 8, 9, 7]] ,
+		'inters' =>  [[4, 6]] ,
+		'Favorite' =>  '4',
+		'Red QPL values'      =>  '9, 7',
+		'Black QPL values'    =>  '4, 6, 8',
+		'Inter QPL' =>  '4, 6, 8, 9, 7',
+	],
+];
---
+++ 0825/2.php
@@ -0,0 +1,112 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [] ,
+		'inters' =>  [] ,
+		'Favorite' =>  '11',
+		'Sure Place'    =>  '',
+		'All Wins values'    =>  '',
+		'Place values'    =>  '',
+		'Red QPL values'      =>  '',
+		'Black QPL values'    =>  '',
+		'Inter Inters' =>  '',
+		'Inter QPL' =>  '',
+		'I' =>  '',
+		'Bet' =>  '',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [] ,
+		'inters' =>  [] ,
+		'Favorite' =>  '2',
+		'Sure Place'    =>  '',
+		'All Wins values'    =>  '',
+		'Place values'    =>  '',
+		'Red QPL values'      =>  '',
+		'Black QPL values'    =>  '',
+		'Inter Inters' =>  '',
+		'Inter QPL' =>  '',
+		'I' =>  '',
+		'Bet' =>  '',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [] ,
+		'inters' =>  [] ,
+		'Favorite' =>  '8',
+		'Sure Place'    =>  '',
+		'All Wins values'    =>  '',
+		'Place values'    =>  '',
+		'Red QPL values'      =>  '',
+		'Black QPL values'    =>  '',
+		'Inter Inters' =>  '',
+		'Inter QPL' =>  '',
+		'I' =>  '',
+		'Bet' =>  '',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [] ,
+		'inters' =>  [] ,
+		'Favorite' =>  '7',
+		'Sure Place'    =>  '',
+		'All Wins values'    =>  '',
+		'Place values'    =>  '',
+		'Red QPL values'      =>  '',
+		'Black QPL values'    =>  '',
+		'Inter Inters' =>  '',
+		'Inter QPL' =>  '',
+		'I' =>  '',
+		'Bet' =>  '',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [] ,
+		'inters' =>  [] ,
+		'Favorite' =>  '2',
+		'Sure Place'    =>  '',
+		'All Wins values'    =>  '',
+		'Place values'    =>  '',
+		'Red QPL values'      =>  '',
+		'Black QPL values'    =>  '',
+		'Inter Inters' =>  '',
+		'Inter QPL' =>  '',
+		'I' =>  '',
+		'Bet' =>  '',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [] ,
+		'inters' =>  [] ,
+		'Favorite' =>  '4',
+		'Sure Place'    =>  '',
+		'All Wins values'    =>  '',
+		'Place values'    =>  '',
+		'Red QPL values'      =>  '',
+		'Black QPL values'    =>  '',
+		'Inter Inters' =>  '',
+		'Inter QPL' =>  '',
+		'I' =>  '',
+		'Bet' =>  '',
+	],
+];
